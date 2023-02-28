1ba6184bedc224d7ab57924aac0bbe3b97c9c2b6
Lucas Alexander Rodrigues Lima <88013886+LuceNori@users.noreply.github.com>
2023-02-28 12:44:26 -0400

    Update Atv05.java

--- tarefas/Atv05.java
+++ tarefas/Atv05.java
@@ -3,23 +3,18 @@ import java.util.Scanner;
 public class Atv05 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Quantidade de linhas:");
-		int k = sc.nextInt();
-		System.out.println("Quantidade de colunas:");
-		int l = sc.nextInt();
-		String m[][]= new String[k][l];
-		for(int i=0;i<k;i++) {
-			sc.nextLine();
-			for(int j=0;j<l;j++) {
-				m[i][j] =sc.nextLine();
+		System.out.println("Caso 5");
+			System.out.println("Quantidade de linhas:");
+			int k = sc.nextInt();
+			System.out.println("Quantidade de colunas:");
+			int l = sc.nextInt();
+			System.out.println("Digite uma palavra");
+			String palavra = sc.nextLine();
+			for(int i=0;i<k;i++) {
+				System.out.println("");
+				for(int j=0;j<l;j++) {
+					System.out.print(palavra+"|");
+				}
 			}
-		}
-		for(int i=0;i<k;i++) {
-			System.out.println("");
-			for(int j=0;j<l;j++) {
-				System.out.print(m[i][j]+"|");
-			}
-		}
 	}
 }
